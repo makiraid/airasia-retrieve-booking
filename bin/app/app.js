5e914c59580b93cea29f133ee42522e3f384f58c
Risky Makira <risky@makira.id>
2020-12-09 18:07:59 +0700

    fix public

--- bin/app/app.js
+++ bin/app/app.js
@@ -13,14 +13,14 @@ const airAsia = require('../modules/airasia/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'../') + "/views");
+app.set('views', path.join(__dirname, '../') + "/views");
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../') + "/views"));
 
 app.use('/', indexRouter, airAsia);
 app.use('/users', usersRouter);
