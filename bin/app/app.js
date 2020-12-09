ad33858705fd98ddc284d92142ac3a416b5a479c
Risky Makira <risky@makira.id>
2020-12-09 18:06:15 +0700

    fix views

--- bin/app/app.js
+++ bin/app/app.js
@@ -13,7 +13,7 @@ const airAsia = require('../modules/airasia/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'../') + "/views");
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
